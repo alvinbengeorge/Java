533b3c4e7b15a46bc6c5b1ae2497bfbd6148d9e6
alvinbengeorge <alvinallen333@gmail.com>
2023-12-22 09:35:47 +0530

    enhanced switch case

---
+++ enhancedSwitchCase.java
@@ -0,0 +1,11 @@
+// FROM JAVA 14 only
+
+public class enhancedSwitchCase {
+
+    public static void main(String[] args) {
+        int a = 5;
+        switch (a) {
+            case 5 -> System.out.println("a is 5");
+        }
+    }
+}
